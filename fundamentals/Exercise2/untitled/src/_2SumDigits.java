4d0125f0b156622e87f1fc1642f09250b9c75346
Stanimir Parvanov <131682926+faranar@users.noreply.github.com>
2023-09-24 15:47:10 +0300

    solved ex2

--- fundamentals/Exercise2/untitled/src/_2SumDigits.java
+++ fundamentals/Exercise2/untitled/src/_2SumDigits.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class _2SumDigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+        while (num!=0){
+            sum += num % 10;
+            num /= 10;
+        }
+        System.out.println(sum);
+    }
+
 }

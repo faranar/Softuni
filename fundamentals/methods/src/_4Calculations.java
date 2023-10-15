bbe70ebfb42b14c977d4baa745ddeef274ee765f
Stanimir Parvanov <131682926+faranar@users.noreply.github.com>
2023-10-15 22:18:32 +0300

    solved lab4 of methods

---
+++ fundamentals/methods/src/_4Calculations.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class _4Calculations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String calculation = scanner.nextLine();
+        int firstNumber = Integer.parseInt(scanner.nextLine());
+        int secondNumber = Integer.parseInt(scanner.nextLine());
+        switch (calculation){
+            case "add":
+                add(firstNumber,secondNumber);
+                break;
+            case "multiply":
+                multiply(firstNumber,secondNumber);
+                break;
+            case "subtract":
+                subtract(firstNumber,secondNumber);
+                break;
+            case "divide":
+                divide(firstNumber,secondNumber);
+                break;
+        }
+    }
+
+    public static void add(int n1, int n2){
+        System.out.println(n1 + n2);
+    }
+    public static void multiply(int n1, int n2){
+        System.out.println(n1 * n2);
+    }
+    public static void subtract(int n1, int n2){
+        System.out.println(n1 - n2);
+    }
+    public static void divide(int n1, int n2){
+        System.out.println(n1 / n2);
+    }
+}
